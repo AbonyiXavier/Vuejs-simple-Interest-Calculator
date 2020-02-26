cf17dab2103cca4ad233a1a0a6ac21031e57d657
AbonyiXavier <francisabonyi@gmail.com>
2020-02-26 11:47:48 +0100

    vuejs simple interest calculator

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Vuejs-simple-Interest-Calculator
+
+# Vuejs CDN script was used to achieve this project
---
+++ app.js
@@ -0,0 +1,30 @@
+new Vue({
+  el: "#vue-app",
+  data: {
+    principal: 0,
+    rate: 0,
+    time: 0,
+    simpleInterest: 0,
+    payBackAmount: 0,
+    ended: false
+  },
+  methods: {
+    calculate(e) {
+      e.preventDefault();
+      this.simpleInterest =
+        Math.round(this.principal * this.rate * this.time) / 100;
+      //   console.log(this.simpleInterest);
+
+      this.payBackAmount =
+        parseFloat(this.principal) + parseFloat(this.simpleInterest);
+    },
+    reset() {
+      this.principal = 0;
+      this.rate = 0;
+      this.time = 0;
+      this.simpleInterest = 0;
+      this.payBackAmount = 0;
+      this.ended = false;
+    }
+  }
+});
